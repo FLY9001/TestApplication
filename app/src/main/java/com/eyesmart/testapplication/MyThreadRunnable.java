bd338a1fcaa491218bd6b9ac24ce292402555a45
FLY <yufei.tian@eyesmart.com.cn>
2017-06-19 09:43:43 +0800

    不知道什么文件

--- app/src/main/java/com/eyesmart/testapplication/MyThreadRunnable.java
+++ app/src/main/java/com/eyesmart/testapplication/MyThreadRunnable.java
@@ -37,7 +37,6 @@ public class MyThreadRunnable implements Runnable {
 
         currentThread.isAlive();                        //判断该线程是否已经启动
         currentThread.join();                           //强制该线程运行，其他线程暂时等待
-        currentThread.interrupt();                      //中断此线程
 
         currentThread.setDaemon(true);                  //设置为守护（后台）线程
         currentThread.setPriority(Thread.MAX_PRIORITY); //设置为最高线程优先级
