2bb9b4edcbc644854da12a459a47f119d4c64ed6
FLY <yufei.tian@eyesmart.com.cn>
2017-07-07 18:11:39 +0800

    handler

--- app/src/main/java/com/eyesmart/testapplication/MainActivity.java
+++ app/src/main/java/com/eyesmart/testapplication/MainActivity.java
@@ -1,13 +1,14 @@
 package com.eyesmart.testapplication;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import com.eyesmart.testapplication.java.APIs;
+import com.eyesmart.testapplication.java.MyCollection;
+import com.eyesmart.testapplication.java.MyEnum;
+import com.eyesmart.testapplication.java.MyGenericity;
+import com.eyesmart.testapplication.java.MyIO;
+import com.eyesmart.testapplication.java.MyThreadRunnable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,37 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        Class[] java = {
+                APIs.class,             //Runtime、System、字符操作、数据日期格式化
+                MyIO.class,             //数据流
+                MyCollection.class,     //集合、数组
+                MyThreadRunnable.class, //线程基础
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+                MyGenericity.class,     //泛型、注解
+                MyEnum.class,           //反射、枚举
+        };
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        Class[] android = {
+                TestActivity.class,     //四大组件、Fragment、Intent
+                TestService.class,
+                TestReceiver.class,
+                TestFragment.class,
 
-        return super.onOptionsItemSelected(item);
+                TestHandler.class,      //线程及消息通讯
+        };
+
     }
 }
---
+++ app/src/main/java/com/eyesmart/testapplication/TestHandler.java
@@ -0,0 +1,71 @@
+package com.eyesmart.testapplication;
+
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+
+/**
+ * Created by Tian on 2017-7-7 0007.
+ */
+
+public class TestHandler {
+    void test() {
+        boolean b = handler.hasMessages(0);     //判断消息队列中是否有此消息
+
+        Message message = new Message();        //创建消息
+        message = handler.obtainMessage(0);
+
+        handler.sendMessage(message);           //发送消息
+        handler.sendMessageDelayed(message, 1000);
+        message.sendToTarget();
+
+        new TestAsyncTask().execute("test");
+    }
+
+    void newThreadTest() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Looper.prepare();                       //准备looper（实例化Looper，创建MessageQueue）
+                Handler threadHandler = new Handler();  //分线程Handler
+                Looper.loop();                          //运行looper
+
+                Handler mainHandler = new Handler(Looper.getMainLooper());//主线程Handler
+            }
+        }).start();
+    }
+
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 0:
+                    break;
+            }
+        }
+    };
+
+    class TestAsyncTask extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            publishProgress("value");
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+    }
+}
