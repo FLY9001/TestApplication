8e8656af629c8ce26731ff8cfe2a0ec40c655dbe
Your Name <you@example.com>
2018-10-24 00:57:39 +0800

    pathEffect

--- app/src/main/java/com/eyesmart/testapplication/android/TestActivity.java
+++ app/src/main/java/com/eyesmart/testapplication/android/TestActivity.java
@@ -1,11 +1,14 @@
 package com.eyesmart.testapplication.android;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+
+import com.eyesmart.testapplication.R;
 
 /**
  * 启动模式（通过AndroidManifest和intent设置）；
@@ -13,11 +16,11 @@ import android.support.v7.app.AppCompatActivity;
  * singleTop：不在栈顶，都会创建新的实例；如新闻类或者阅读类App的内容页面
  * singleTask：重用时，会让该实例回到栈顶，它上面的实例将会被移出栈(会调用实例的onNewIntent())；如浏览器的主界面
  * singleInstance：任何应用再激活该Activity时都会重用该栈中的实例( 会调用实例的 onNewIntent())；如闹铃提醒
- *
+ * <p>
  * 清空任务栈：通过AndroidManifest和intent设置
  */
 
-public class TestActivity extends AppCompatActivity {
+public class TestActivity extends Activity {
 
     void test() {
         onCreate(null);
@@ -37,6 +40,20 @@ public class TestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //设置Window属性
+        TestWindow.activityWindow(this);
+
+        //最终调用PhoneWindow的setContentView
+        //1、得到DecorView，其是Activity的顶级根View，包括标题栏(ActionBar)和内容栏(contentParent)
+        //2、设置DecorView内容栏的View，即contentView，可通过findViewById(android.R.id.content)获得，content
+        //3、在onResume中调用WindowManager的addView方法，接着setVisibility显示出来
+        setContentView(R.layout.activity_main);
+
+        //可通过android.R.id.content获得contentView
+        FrameLayout content = (FrameLayout) findViewById(android.R.id.content);
+        //此为我们为activity设置的View
+        content.getChildAt(0);
+
         if (savedInstanceState != null) {                               //onCreate()中的savedInstanceState有可能为空
             savedInstanceState.getString("key");
         }
@@ -67,5 +84,6 @@ public class TestActivity extends AppCompatActivity {
     @Override
     protected void onNewIntent(Intent intent) {                         //配合启动模式
         super.onNewIntent(intent);
+
     }
 }
--- app/src/main/java/com/eyesmart/testapplication/android/TestWindow.java
+++ app/src/main/java/com/eyesmart/testapplication/android/TestWindow.java
@@ -1,33 +1,61 @@
 package com.eyesmart.testapplication.android;
 
+import android.app.Activity;
 import android.content.Context;
 import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
 
 /**
- * Created by FLY on 2018/10/23 0023.
+ * Window，通过Activity、Dialog、Toast、自定义的视图（悬浮窗） 呈现
+ * https://www.cnblogs.com/deman/p/5641765.html
  */
 
 public class TestWindow {
     public static void creatWindow(Context context) {
+        /**WindowManager*/
         WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+
+        /**LayoutParams*/
         WindowManager.LayoutParams wmParams = new WindowManager.LayoutParams();
-        //type
-        wmParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
+        //wmParams = ((Activity) context).getWindow().getAttributes();  //activity获取
 
-        //flags，悬浮窗不获得焦点,不限制边界
-        wmParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS;
-
+        //type，窗口类型；1~2999，层级越大越顶层
+        //分为 应用Window（1~99）、子Window（1000~1999，需附属父Window）、系统window（2000~2999，需声明权限）
+        //android.permission.SYSTEM_ALERT_WINDOW 允许程序显示系统窗口
+        wmParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY;
+        //flags，显示特性； https://blog.csdn.net/qq_28535319/article/details/82790331
+        wmParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+                | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED;     //锁屏界面
         //设置位置，以屏幕左上角为原点
         wmParams.gravity = Gravity.LEFT | Gravity.TOP;
         //wmParams.x = 0;
         //wmParams.y = 0;
-        // 置长宽
+        //设置长宽
         wmParams.width = WindowManager.LayoutParams.MATCH_PARENT;
         wmParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
         wmParams.format = 1;
 
+        /**add、update、remove*/
         wm.addView(new View(context), wmParams);
+        wm.updateViewLayout(new View(context), wmParams);
+        wm.removeView(new View(context));
+        //((Activity) context).getWindow().setAttributes(wmParams);     //activity设置
+    }
+
+    /**
+     * 1、activity
+     */
+    public static void activityWindow(Activity activity) {
+        WindowManager.LayoutParams wmParams = activity.getWindow().getAttributes();  //获取window
+        activity.getWindow().setAttributes(wmParams);                                //设置window
+
+        activity.getWindow().getDecorView();                                         //获取DecorView
+
+        //activity与Window相关的生命周期回调
+        activity.onAttachedToWindow();
+        activity.onDetachedFromWindow();
+        activity.dispatchTouchEvent(null);
     }
 }
