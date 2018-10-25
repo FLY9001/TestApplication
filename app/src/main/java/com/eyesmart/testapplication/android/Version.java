d3d536ebd04f90515fe06cc95cbf2509901a447d
Your Name <you@example.com>
2018-10-26 01:08:53 +0800

    pathEffect

---
+++ app/src/main/java/com/eyesmart/testapplication/android/Version.java
@@ -0,0 +1,27 @@
+package com.eyesmart.testapplication.android;
+
+import com.eyesmart.testapplication.ui.viewwidget.RecyclerFragment;
+
+/**
+ * Created by 1 on 2018/10/25 0025.
+ */
+
+public class Version {
+    /**
+     * Android5.0 新特性：
+     * 1、Material Design
+     * 2、支持多种设备，如平板、手表、智能电视、汽车
+     * 3、全新的通知中心
+     * 4、64位ART虚拟机
+     * 5、Overview多任务窗口
+     * 6、设备识别解锁
+     * 7、Ok Google语音命令
+     * 8、Face unlock面部解锁
+     */
+    void version5() {
+        //RecyclerFragment
+        new RecyclerFragment();
+        //CardView，继承FrameLayout，可设置 圆角和阴影，具有立体性
+        //Notification，3种
+    }
+}
--- app/src/main/java/com/eyesmart/testapplication/ui/viewwidget/RecyclerFragment.java
+++ app/src/main/java/com/eyesmart/testapplication/ui/viewwidget/RecyclerFragment.java
@@ -5,6 +5,9 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
@@ -21,11 +24,13 @@ import java.util.List;
 
 
 /**
- * A simple {@link Fragment} subclass.
+ * RecyclerView：高度解耦、更灵活、更效率
+ * 替代ListView、GridView
+ * 需导入support-v7包
  */
 public class RecyclerFragment extends Fragment {
     private View rootView;
-    RecyclerView rv_recycler;
+    private RecyclerView rv_recycler;
 
     public RecyclerFragment() {
         // Required empty public constructor
@@ -45,54 +50,59 @@ public class RecyclerFragment extends Fragment {
             }
         }
 
-        //设置LayoutManager
+        //设置LayoutManager，LinearLayoutManager线性排列（垂直、水平），GridLayoutManager网格，StaggeredGridLayoutManager瀑布流
         rv_recycler.setLayoutManager(new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL));
+        //设置分割线
+        rv_recycler.addItemDecoration(new DividerItemDecoration(getContext(), LinearLayoutManager.VERTICAL));
+        //设置item增加和删除时动画
+        rv_recycler.setItemAnimator(new DefaultItemAnimator());
         //如果可以确定每个item的高度是固定的，设置这个选项可以提高性能
         rv_recycler.setHasFixedSize(true);
- //       rv_recycler.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+
         RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(beens);
+        //设置item点击事件
+        recyclerViewAdapter.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Toast.makeText(getContext(), "点击了第" + position + "个", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onItemLongClick(View view, int position) {
+                Toast.makeText(getContext(), "长点击了第" + position + "个", Toast.LENGTH_SHORT).show();
+            }
+        });
         rv_recycler.setAdapter(recyclerViewAdapter);
         return rootView;
     }
 
     class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         private List<Bean> mBeans;
+        private OnItemClickListener mOnItemClickListener;
 
         public RecyclerViewAdapter(List<Bean> beans) {
             mBeans = beans;
         }
 
+        //加载条目布局
         @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(final ViewGroup parent, int viewType) {
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             switch (viewType) {
                 case Bean.TYPE_RECEIVED:
                     View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_left, parent, false);
-                    final ViewHolder viewHolder = new ViewHolder(itemView);
-                    viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            int adapterPosition = viewHolder.getAdapterPosition();
-                            Toast.makeText(v.getContext(), "点击了第" + adapterPosition + "个", Toast.LENGTH_SHORT).show();
-                        }
-                    });
+                    ViewHolder viewHolder = new ViewHolder(itemView);
                     return viewHolder;
                 case Bean.TYPE_SENT:
                     View itemView2 = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_right, parent, false);
-                    final ViewHolder2 viewHolder2 = new ViewHolder2(itemView2);
-                    viewHolder2.itemView.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            int adapterPosition = viewHolder2.getAdapterPosition();
-                            Toast.makeText(v.getContext(), "点击了第" + adapterPosition + "个", Toast.LENGTH_SHORT).show();
-                        }
-                    });
+                    ViewHolder2 viewHolder2 = new ViewHolder2(itemView2);
                     return viewHolder2;
             }
             return null;
         }
 
+        //绑定条目数据
         @Override
-        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        public void onBindViewHolder(final RecyclerView.ViewHolder holder, int position) {
             if (holder instanceof RecyclerViewAdapter.ViewHolder) {
                 ((RecyclerViewAdapter.ViewHolder) holder).icon.setImageBitmap(mBeans.get(position).getIcon());
                 ((RecyclerViewAdapter.ViewHolder) holder).text.setText(mBeans.get(position).getText());
@@ -100,6 +110,33 @@ public class RecyclerFragment extends Fragment {
                 ((RecyclerViewAdapter.ViewHolder2) holder).icon.setImageBitmap(mBeans.get(position).getIcon());
                 ((RecyclerViewAdapter.ViewHolder2) holder).text.setText(mBeans.get(position).getText());
             }
+            //设置点击监听
+            if (mOnItemClickListener != null) {
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        int position = holder.getLayoutPosition();
+                        mOnItemClickListener.onItemClick(holder.itemView, position);
+                    }
+                });
+                holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View v) {
+                        int position = holder.getLayoutPosition();
+                        mOnItemClickListener.onItemLongClick(holder.itemView, position);
+                        return false;
+                    }
+                });
+
+                //改变布局参数，可设置瀑布流效果
+                ViewGroup.LayoutParams lp = holder.itemView.getLayoutParams();
+                lp.height = 0;
+                holder.itemView.setLayoutParams(lp);
+            }
+        }
+
+        public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+            mOnItemClickListener = onItemClickListener;
         }
 
         @Override
@@ -137,6 +174,12 @@ public class RecyclerFragment extends Fragment {
                 text = (TextView) itemView.findViewById(R.id.tv_right_listView);
             }
         }
+    }
+
+    interface OnItemClickListener {
+        void onItemClick(View view, int position);
+
+        void onItemLongClick(View view, int position);
     }
 
     /**
